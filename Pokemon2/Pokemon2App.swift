cd51ae2a5d9c2ae1168f0d4a54f9512271361fd5
William Moraes <william.silva@e-unicred.com.br>
2025-06-13 07:45:51 -0300

    Initial Commit

---
+++ Pokemon2/Pokemon2App.swift
@@ -0,0 +1,17 @@
+//
+//  Pokemon2App.swift
+//  Pokemon2
+//
+//  Created by William Moraes da Silva on 13/06/25.
+//
+
+import SwiftUI
+
+@main
+struct Pokemon2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
