d7d009cb87a06529f3cb72ecc7fa4f53d1d310da
William Moraes <william.silva@e-unicred.com.br>
2025-06-13 09:14:08 -0300

    feat(data): implement PokemonRepositoryImpl and datasource

---
+++ Pokemon2/Data/DataSource/PokemonRemoteDataSource.swift
@@ -0,0 +1,24 @@
+//
+//  Untitled.swift
+//  Pokemon2
+//
+//  Created by William Moraes da Silva on 13/06/25.
+//
+
+class PokemonRemoteDataSource {
+    private let networkService: NetworkService
+    
+    init(networkService: NetworkService = .shared) {
+        self.networkService = networkService
+    }
+
+    func fetchPokemonList(offset: Int, limit: Int) async throws -> PokemonListDTO {
+        let endpoint = PokeAPIEndpoint.getPokemonList(offset: offset, limit: limit)
+        return try await networkService.request(endpoint: endpoint)
+    }
+    
+    func fetchPokemonDetail(id: Int) async throws -> PokemonDTO {
+        let endpoint = PokeAPIEndpoint.getPokemonDetail(id: id)
+        return try await networkService.request(endpoint: endpoint)
+    }
+}
