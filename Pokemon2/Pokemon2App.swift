8125c4bcb6b433df9fab5aa266db91374fac979d
William Moraes <william.silva@e-unicred.com.br>
2025-06-13 09:54:57 -0300

    chore: configure app entry point and dependency injection

--- Pokemon2/Pokemon2App.swift
+++ Pokemon2/Pokemon2App.swift
@@ -9,10 +9,22 @@ import SwiftUI
 
 @main
 struct Pokemon2App: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  @StateObject private var pokedexViewModel: PokedexViewModel
+  
+  init() {
+    // creating Data Layer
+    let remoteDataSource = PokemonRemoteDataSource()
+    let pokemonRepository: PokemonRepository = PokemonRepositoryImpl(remoteDataSource: remoteDataSource)
+    
+    // creating Data Layer (Use Case)
+    let getPokemonListUseCase = GetPokemonListUseCase(repository: pokemonRepository)
+    
+    _pokedexViewModel = StateObject(wrappedValue: PokedexViewModel(getPokemonListUseCase: getPokemonListUseCase))
+  }
+  
+  var body: some Scene {
+    WindowGroup {
+      PokedexView(viewModel: pokedexViewModel)
     }
+  }
 }
-
